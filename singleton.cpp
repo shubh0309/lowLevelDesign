d26770358348a08c52632a73fba0017b6ab07d5d
shubh0309 <87462228+shubh0309@users.noreply.github.com>
2026-01-12 11:07:06 +0530

    Refactor Singleton class for proper singleton pattern
    Refactor Singleton class to implement the singleton pattern correctly by making the constructor private and providing a static method to get the instance.

--- singleton.cpp
+++ singleton.cpp
@@ -1,15 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Singleton{
-    public:
+    private:
+        static Singleton* instance;
         Singleton(){
             cout<<"Singleton constructor called. New Object created.\n";
         }
+    public:
+        static Singleton* getInstance(){
+            if(instance == nullptr)
+                instance = new Singleton();
+            return instance;
+        }
 };
 
+Singleton* Singleton:: instance = nullptr;
+
 int main(){
-    Singleton *s1 = new Singleton();
-    Singleton *s2 = new Singleton();
+    Singleton *s1 = Singleton :: getInstance();
+    Singleton *s2 = Singleton :: getInstance();
     cout<< (s1 == s2) <<endl;
     return 0;
 }
