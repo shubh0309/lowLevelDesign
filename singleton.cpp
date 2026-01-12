0ffcf1ca915a75c554a1206960b0d3ff251a4cd4
shubh0309 <87462228+shubh0309@users.noreply.github.com>
2026-01-12 11:17:25 +0530

    Make Singleton class thread-safe
    Implemented thread-safe singleton pattern using mutex.

--- singleton.cpp
+++ singleton.cpp
@@ -3,19 +3,25 @@ using namespace std;
 class Singleton{
     private:
         static Singleton* instance;
+        static mutex mtx;
         Singleton(){
             cout<<"Singleton constructor called. New Object created.\n";
         }
     public:
         static Singleton* getInstance(){
-            if(instance == nullptr)
-                instance = new Singleton();
+            if(instance == nullptr){
+                lock_guard<mutex> lock(mtx);
+                if(instance == nullptr){
+                    instance = new Singleton();
+                }
+            }
+                
             return instance;
         }
 };
 
 Singleton* Singleton:: instance = nullptr;
-
+mutex Singleton :: mtx;
 int main(){
     Singleton *s1 = Singleton :: getInstance();
     Singleton *s2 = Singleton :: getInstance();
